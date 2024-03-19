a7fd1f94a5df9eec3e9155e13337bc966036fd50
Gicherujoh <johnwambugu236@gmail.com>
2024-03-19 08:07:07 +0300

    Removed Frontend folder

--- Frontend/src/Redux/Store.jsx
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import ProductSlice from './ProductSlice';
-export const store = configureStore({
-    reducer: {
-        products:ProductSlice
-    }
-})
